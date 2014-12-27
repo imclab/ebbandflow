47773becbe4060498a5c4de7c6f7e70b35a9706e
legshampoo <daniel.c.baker@gmail.com>
2014-12-27 02:09:05 -0500

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var request = require('request');
+//var http = require('http');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+//server.listen(1337);
+
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static('src/public'));
+
+server.listen(1337, function() {
+	console.log('listening on *:1337');
+});
+
+server.on('connection', function(socket) {
+	console.log('new connection');
+});
